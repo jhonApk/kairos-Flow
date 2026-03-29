6eecffaf9fae395ad4df8a8d6ea192f0a257c9f2
msantos <devjhon.mobile@gmail.com>
2026-03-29 15:54:18 -0300

    pojeto inicial, construçãode telaS

---
+++ app/src/main/java/com/devjhon/kairosflowapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.devjhon.kairosflowapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.devjhon.kairosflowapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+        initNavigation()
+    }
+
+
+
+    private fun initNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.btnv, navController)
+    }
+}
